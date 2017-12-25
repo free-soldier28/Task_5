d2b4abb555d68806052795f63768faa3b1366729
roman <romik280h@gmail.com>
2017-12-25 20:49:54 +0300

    Added the entity SalesViewModel in a web project

---
+++ WindowsService/SalesWebApplication/Models/SalesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalesWebApplication.Models
+{
+    public class SalesViewModel
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string ManagerName { get; set; }
+        public string CustomerName { get; set; }
+        public string ProductName { get; set; }
+    }
+}

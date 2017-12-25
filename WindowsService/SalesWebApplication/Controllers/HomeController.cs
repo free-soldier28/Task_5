49a992fe97dcf377abb4ffea759835e60c4c5bab
roman <romik280h@gmail.com>
2017-12-25 20:56:19 +0300

    Has deduced on the main form the table with the data of sales.

--- WindowsService/SalesWebApplication/Controllers/HomeController.cs
+++ WindowsService/SalesWebApplication/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using WindowsService.BLL.DTO;
+using WindowsService.BLL.Interfaces;
+using AutoMapper;
+using SalesWebApplication.Models;
 
 namespace SalesWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private ISalesService _salesService;
+
+        public HomeController(ISalesService alesService)
+        {
+           _salesService = alesService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+           var allSales = Mapper.Map<IEnumerable<SalesViewModel>>(_salesService.GetSales());
+            return View(allSales);
         }
 
         public ActionResult About()
